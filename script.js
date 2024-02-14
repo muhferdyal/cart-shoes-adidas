3e5b2ec117a40f1a6573d85a51654821a7cd2936
muhferdyal <136937286+muhferdyal@users.noreply.github.com>
2024-02-14 12:30:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const $options = document.querySelectorAll('.color-option .circle');
+let $activeOption = document.querySelector('.color-option .circle.active');
+let $activeImage = document.querySelector('.main-images .active');
+
+const handleOptionClick = (event) => {
+  $activeOption.classList.remove('active');
+  $activeOption = event.target;
+  $activeOption.classList.add('active');
+  
+  const option = $activeOption.dataset.option;
+  $activeImage.classList.remove('active');
+  $activeImage = document.querySelector(`.main-images .${option}`);
+  $activeImage.classList.add('active');
+}
+
+$options.forEach(($ele) => {
+  $ele.addEventListener('click', handleOptionClick);
+});
